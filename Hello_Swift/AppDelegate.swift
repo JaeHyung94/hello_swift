4ffcd015f5c705a09f451af41cc55d8ffb09f403
Jay Park <jhpark.main@gmail.com>
2022-07-27 22:49:47 +0900

    Alert

--- Hello_Swift/AppDelegate.swift
+++ Hello_Swift/AppDelegate.swift
@@ -8,7 +8,7 @@
 import UIKit
 import CoreData
 
-g@main
+@main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
--- Hello_Swift/ViewController.swift
+++ Hello_Swift/ViewController.swift
@@ -7,44 +7,83 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-    let NUM_COLUMN = 2
-    var imageNameArray = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg"]
+class ViewController: UIViewController {
+    let imageOn = UIImage(named: "lamp-on.png")
+    let imageOff = UIImage(named: "lamp-off.png")
+    let imageRemove = UIImage(named: "lamp-remove.png")
     
-    @IBOutlet var imagePicker: UIPickerView!
-    @IBOutlet var labelSelectedItem: UILabel!
+    var isOn = true
+    
     @IBOutlet var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.imagePicker.delegate = self
-        self.imagePicker.dataSource = self
         
-        
-        labelSelectedItem.text = imageNameArray[0]
-        imageView.image = UIImage(named: imageNameArray[0])
+        imageView.image = imageOn
     }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return imageNameArray.count
-    }
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return NUM_COLUMN
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return imageNameArray[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        let target = imageNameArray[row]
-        //몇 번째 column에서 온 건지는 component로 구분
-        if(component == 0) {
-            labelSelectedItem.text = target
+
+    @IBAction func handleOn (_ sender: UIButton) {
+        if(isOn) {
+            let lampOnAlert = UIAlertController(title: "전구를 켤 수 없습니다.", message: "이미 켜진 전구입니다.", preferredStyle: UIAlertController.Style.alert)
+            
+            let onAction = UIAlertAction(title: "넹", style: UIAlertAction.Style.default, handler: nil)
+            
+            lampOnAlert.addAction(onAction)
+            present(lampOnAlert, animated: true, completion: nil)
         } else {
-            imageView.image = UIImage(named: target)
+            imageView.image = imageOn
+            isOn = true
         }
+    }
+    
+    @IBAction func handleOff (_ sender: UIButton) {
+        if(isOn) {
+            let lampOffAlert = UIAlertController(title: "전구 끄기", message: "전구를 끄시겠어요?", preferredStyle: UIAlertController.Style.alert)
+            
+            let onAction = UIAlertAction(title: "꺼 죠", style: UIAlertAction.Style.default, handler: {
+                ACTION in self.imageView.image = self.imageOff
+                self.isOn = false
+            })
+            
+            let cancelAction = UIAlertAction(title: "아니용", style: UIAlertAction.Style.destructive, handler: nil)
+            
+            lampOffAlert.addAction(cancelAction)
+            lampOffAlert.addAction(onAction)
+            
+            present(lampOffAlert, animated: true, completion: nil)
+        } else {
+            let lampOffAlert = UIAlertController(title: "전구를 끌 수 없습니다.", message: "이미 꺼진 전구입니다.", preferredStyle: UIAlertController.Style.alert)
+            
+            let onAction = UIAlertAction(title: "넹!", style: UIAlertAction.Style.destructive, handler: nil)
+            
+            lampOffAlert.addAction(onAction)
+            present(lampOffAlert, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func handleRemove (_ sender: UIButton) {
+        let lampRemoveAlert = UIAlertController(title: "전구 제거", message: "전구를 제거하시겠어요?", preferredStyle: UIAlertController.Style.alert)
+        
+        let lampOnAction = UIAlertAction(title: "전구 켜기", style: UIAlertAction.Style.default, handler: {
+            ACTION in self.imageView.image = self.imageOn
+            self.isOn = true
+        })
+        
+        let lampOffAction = UIAlertAction(title: "전구 끄기", style: UIAlertAction.Style.default, handler: {
+            ACTION in self.imageView.image = self.imageOff
+            self.isOn = false
+        })
+        
+        let lampRemoveAction = UIAlertAction(title: "전구 제거", style: UIAlertAction.Style.default, handler: {
+            ACTION in self.imageView.image = self.imageRemove
+            self.isOn = false
+        })
+        
+        lampRemoveAlert.addAction(lampOnAction)
+        lampRemoveAlert.addAction(lampOffAction)
+        lampRemoveAlert.addAction(lampRemoveAction)
+        
+        present(lampRemoveAlert, animated: true, completion: nil)
     }
 }
