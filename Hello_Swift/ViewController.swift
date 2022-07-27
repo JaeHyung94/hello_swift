64e9117540f3ecab8cbfd0f537963c482a349853
Jay Park <jhpark.main@gmail.com>
2022-07-27 17:00:10 +0900

    Picker View

--- Hello_Swift/ViewController.swift
+++ Hello_Swift/ViewController.swift
@@ -7,44 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    let timeSelector: Selector = #selector(ViewController.checkTimer)
-    var selectedDate: Date!
-    var currentDate: Date!
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    let MAX_ARRAY_NUM = 10
+    let PICKER_VIEW_COLUMN = 1
+    let PICKER_HEIGHT = 80.0
+    var imageFileName = ["1.jpg", "2.jpg", "3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg", "10.jpg"]
     
-    @IBOutlet var labelCurrentDate: UILabel!
-    @IBOutlet var labelSelectedDate: UILabel!
+    @IBOutlet var labelSelectedItem: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var pickerView: UIPickerView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: timeSelector, userInfo: nil, repeats: true)
     }
     
-    @IBAction func handleSelectDate(_ sender: UIDatePicker) {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
-        
-        selectedDate = sender.date
-            labelSelectedDate.text = "선택 시간: " + formatter.string(from: selectedDate)
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return PICKER_VIEW_COLUMN
     }
     
-    @objc func checkTimer() {
-        let formatter = DateFormatter()
-        currentDate = Date()
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return imageFileName.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return PICKER_HEIGHT
+    }
+    
+    //이미지 이름을 피커뷰에 보이고 싶은 경우
+//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//        return imageFileName[row]
+//    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let imageView = UIImageView(image: UIImage(named: imageFileName[row]))
+        imageView.frame = CGRect(x: 0, y: 0, width: 100, height: 150)
         
-        formatter.locale = Locale(identifier: "ko-KR")
-        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
-        
-        labelCurrentDate.text = "현재 시간: " + formatter.string(from: currentDate)
-
-        if(selectedDate != nil && currentDate >= (selectedDate + 60)) {
-            self.view.backgroundColor = UIColor.white
-        } else if (selectedDate != nil && currentDate > selectedDate) {
-            self.view.backgroundColor = UIColor.red
-        } else {
-            self.view.backgroundColor = UIColor.white
-        }
+        return imageView
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        labelSelectedItem.text = imageFileName[row]
+        imageView.image = UIImage(named: imageFileName[row])
     }
 }
 
