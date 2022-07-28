fa30519813c778353d8d69c7050c5818c085c1fa
Jay Park <jhpark.main@gmail.com>
2022-07-28 16:42:08 +0900

    Map View

--- Hello_Swift/ViewController.swift
+++ Hello_Swift/ViewController.swift
@@ -6,43 +6,93 @@
 //
 
 import UIKit
-import WebKit
+import MapKit
 
-class ViewController: UIViewController, WKNavigationDelegate {
-    @IBOutlet var webView: WKWebView!
-    @IBOutlet var indicator: UIActivityIndicatorView!
+class ViewController: UIViewController, CLLocationManagerDelegate{
+    @IBOutlet var mapView: MKMapView!
+    @IBOutlet var labelTitle: UILabel!
+    @IBOutlet var labelAddress: UILabel!
     
-    func loadWebView(url: String) {
-        let targetUrl = URL(string: url)
-        let request = URLRequest(url: targetUrl!)
-        
-        webView.load(request)
-    }
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        webView.navigationDelegate = self
         
-        let path = Bundle.main.path(forResource: "samplehtml", ofType: "html")
-        let targetUrl = URL(fileURLWithPath: path!)
-        let request = URLRequest(url: targetUrl)
+        labelTitle.text = ""
+        labelAddress.text = ""
         
-        webView.load(request)
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        mapView.showsUserLocation = true
     }
     
-    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
-        indicator.startAnimating()
-        indicator.isHidden = false
+    func goLocation(latitude: CLLocationDegrees, longitude: CLLocationDegrees, delta span:Double) -> CLLocationCoordinate2D {
+        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)
+        let region = MKCoordinateRegion(center: location, span: spanValue)
+        
+        mapView.setRegion(region, animated: true)
+        
+        return location
     }
     
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        indicator.stopAnimating()
-        indicator.isHidden = true
+    func setAnnotation(latitude: CLLocationDegrees, longitude: CLLocationDegrees, delta span: Double, title: String, subtitle: String) {
+        let annotation = MKPointAnnotation()
+        
+        annotation.coordinate = goLocation(latitude: latitude, longitude: longitude, delta: span)
+        
+        annotation.title = title
+        annotation.subtitle = subtitle
+        mapView.addAnnotation(annotation)
     }
     
-    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
-        indicator.stopAnimating()
-        indicator.isHidden = true
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last
+        _ = goLocation(latitude: (location?.coordinate.latitude)!, longitude: (location?.coordinate.latitude)!, delta: 0.01)
+        
+        CLGeocoder().reverseGeocodeLocation(location!, completionHandler: {
+            (placemarks, error) -> Void in
+            let pm = placemarks!.first
+            let country = pm!.country
+            var address:String = country!
+            
+            if pm!.locality != nil {
+                address += " "
+                address += pm!.locality!
+            }
+            
+            if pm!.thoroughfare != nil {
+                address += " "
+                address += pm!.thoroughfare!
+            }
+            
+            self.labelTitle.text = "현재 위치"
+            self.labelAddress.text = address
+        })
+        
+        locationManager.stopUpdatingLocation()
     }
+
+    @IBAction func handleSelectLocation(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            self.labelTitle.text = "현재 위치"
+            self.labelAddress.text = ""
+            locationManager.startUpdatingLocation()
+        } else if sender.selectedSegmentIndex == 1 {
+            setAnnotation(latitude: 37.57, longitude: 126.9081, delta: 1, title: "집", subtitle: "대림아파트")
+            
+            self.labelTitle.text = "보고 계신 위치"
+            self.labelAddress.text = "월드타운 대림아파트"
+        } else if sender.selectedSegmentIndex == 2 {
+            setAnnotation(latitude: 37.5634, longitude: 126.9347, delta: 1, title: "학교", subtitle: "연세대학교 과학원")
+            
+            self.labelTitle.text = "보고 계신 위치"
+            self.labelAddress.text = "연세대학교 과학원"
+        }
+    }
+    
   }
