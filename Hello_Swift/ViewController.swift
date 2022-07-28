f41a99f6c561add13b25cf1681516f74e3b9a108
Jay Park <jhpark.main@gmail.com>
2022-07-28 13:57:38 +0900

    Challenge: Webview

--- Hello_Swift/ViewController.swift
+++ Hello_Swift/ViewController.swift
@@ -9,13 +9,11 @@ import UIKit
 import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate {
-    @IBOutlet var urlInput: UITextField!
     @IBOutlet var webView: WKWebView!
     @IBOutlet var indicator: UIActivityIndicatorView!
     
-    func loadWebPage(_ url: String) {
-        let target = checkURL(url: url)
-        let targetUrl = URL(string: target)
+    func loadWebView(url: String) {
+        let targetUrl = URL(string: url)
         let request = URLRequest(url: targetUrl!)
         
         webView.load(request)
@@ -25,7 +23,12 @@ class ViewController: UIViewController, WKNavigationDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         webView.navigationDelegate = self
-        loadWebPage("https://naver.com")
+        
+        let path = Bundle.main.path(forResource: "samplehtml", ofType: "html")
+        let targetUrl = URL(fileURLWithPath: path!)
+        let request = URLRequest(url: targetUrl)
+        
+        webView.load(request)
     }
     
     func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
@@ -42,59 +45,4 @@ class ViewController: UIViewController, WKNavigationDelegate {
         indicator.stopAnimating()
         indicator.isHidden = true
     }
-    
-    func checkURL(url: String) -> String {
-        var target = url
-        let flag = target.hasPrefix("https://")
-        
-        if !flag {
-            target = "https://" + target
-        }
-        
-        return target
-    }
-
-    @IBAction func handleSubmit(_ sender: UIButton) {
-        let target = checkURL(url: urlInput.text!)
-        urlInput.text = ""
-        loadWebPage(target)
-    }
-    
-    @IBAction func handleSite1Button(_ sender: UIButton) {
-        loadWebPage("https://yonsei.ac.kr")
-    }
-    
-    @IBAction func handleSite2Button(_ sender: UIButton) {
-        loadWebPage("https://velog.io")
-    }
-    
-    @IBAction func handleHTMLButton(_ sender: UIButton) {
-        let htmlString = "<h1>This is htmlString</h1><a href=\"https://daum.net\">daum.net으로 이동</a>"
-        
-        webView.loadHTMLString(htmlString, baseURL: nil)
-    }
-    
-    @IBAction func handleFileButton(_ sender: UIButton) {
-        let path = Bundle.main.path(forResource: "samplehtml", ofType: "html")
-        let url = URL(fileURLWithPath: path!)
-        let request = URLRequest(url: url)
-        
-        webView.load(request)
-    }
-    
-    @IBAction func handleCancel(_ sender: UIBarButtonItem) {
-        webView.stopLoading()
-    }
-    
-    @IBAction func handleRefresh(_ sender: UIBarButtonItem) {
-        webView.reload()
-    }
-    
-    @IBAction func handleGoBack(_ sender: UIBarButtonItem) {
-        webView.goBack()
-    }
-    
-    @IBAction func handleGoForward(_ sender: UIBarButtonItem) {
-        webView.goForward()
-    }
-}
+  }
