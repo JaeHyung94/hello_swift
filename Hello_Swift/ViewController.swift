f7fd072a582ef0ac0fb9b87341cac3092a964c7d
Jay Park <jhpark.main@gmail.com>
2022-07-30 00:43:04 +0900

    core graphics

--- Hello_Swift/ViewController.swift
+++ Hello_Swift/ViewController.swift
@@ -7,14 +7,8 @@
 
 import UIKit
 
-class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-    @IBOutlet var imageView1: UIImageView!
-    @IBOutlet var imageView2: UIImageView!
-    @IBOutlet var imageView3: UIImageView!
-    
-    let imagePicker = UIImagePickerController()
-    var numImage = 0
-    var capturedImage: UIImage!
+class ViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,63 +16,96 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func handleCamera(_ sender: UIButton) {
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            if numImage < 3 {
-                numImage += 1
-            } else {
-                numImage = 0
-            }
-            
-            imagePicker.delegate = self
-            imagePicker.sourceType = .camera
-            imagePicker.mediaTypes = ["public.image"]
-            imagePicker.allowsEditing = false
-            
-            present(imagePicker, animated: true, completion: nil)
-        }
-    }
-    
-    @IBAction func handleLibrary(_ sender: UIButton) {
-        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
-            if numImage < 3 {
-                numImage += 1
-            } else {
-                numImage = 1
-            }
-            
-            imagePicker.delegate = self
-            imagePicker.sourceType = .photoLibrary
-            imagePicker.mediaTypes = ["public.image"]
-            imagePicker.allowsEditing = false
-            
-            present(imagePicker, animated: true, completion: nil)
-        }
-    }
-    
-    @IBAction func handleReset(_ sender: UIButton) {
-        imageView1.image = nil
-        imageView2.image = nil
-        imageView3.image = nil
-        numImage = 0
-    }
-    
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        numImage -= 1
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        capturedImage = (info[UIImagePickerController.InfoKey.originalImage] as! UIImage)
+    @IBAction func handleLine(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(imageView.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
         
-        if numImage == 1 {
-            imageView1.image = capturedImage
-        } else if numImage == 2 {
-            imageView2.image = capturedImage
-        } else if numImage == 3 {
-            imageView3.image = capturedImage
-        }
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.red.cgColor)
         
-        self.dismiss(animated: true, completion: nil)
+        context.move(to: CGPoint(x: 70, y: 50))
+        context.addLine(to: CGPoint(x: 270, y: 250))
+        
+        context.strokePath()
+        
+        context.setLineWidth(4.0)
+        context.setStrokeColor(UIColor.blue.cgColor)
+        
+        context.move(to: CGPoint(x: 170, y: 200))
+        context.addLine(to: CGPoint(x: 270, y: 350))
+        context.addLine(to: CGPoint(x: 70, y: 350))
+        context.addLine(to: CGPoint(x: 170, y: 200))
+        
+        context.strokePath()
+        
+        imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
     }
-}
+    
+    @IBAction func handleRect(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(imageView.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.cyan.cgColor)
+        
+        context.move(to: CGPoint(x: 70, y: 70))
+        context.addLine(to: CGPoint(x: 70, y: 170))
+        context.addLine(to: CGPoint(x: 170, y: 170))
+        context.addLine(to: CGPoint(x: 170, y: 70))
+        context.addLine(to: CGPoint(x: 70, y: 70))
+        
+        context.strokePath()
+        
+        imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+    @IBAction func handleCircle(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(imageView.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.purple.cgColor)
+        
+        context.move(to: CGPoint(x: 100, y: 100))
+        context.addEllipse(in: CGRect(x: 70, y: 50, width: 200, height: 100))
+        context.strokePath()
+        
+        imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+    @IBAction func handleArc(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(imageView.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.green.cgColor)
+        
+        context.move(to: CGPoint(x: 120, y: 250))
+        context.addArc(tangent1End: CGPoint(x: 250, y: 50), tangent2End: CGPoint(x: 250, y: 250), radius: CGFloat(50))
+        context.strokePath()
+        
+        imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+    @IBAction func handleFill(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(imageView.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        
+        context.setLineWidth(4.0)
+        context.setStrokeColor(UIColor.orange.cgColor)
+        context.setFillColor(UIColor.red.cgColor)
+        
+        let rect = CGRect(x: 70, y: 50, width: 200, height: 100)
+        context.addRect(rect)
+        context.fill(rect)
+        context.strokePath()
+        
+        imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+    }
+ }
