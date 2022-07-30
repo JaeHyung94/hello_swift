b200e46d05897a40562b5e0f4ce2ca52725a0f53
Jay Park <jhpark.main@gmail.com>
2022-07-30 19:42:33 +0900

    Sketch app

--- Hello_Swift/ViewController.swift
+++ Hello_Swift/ViewController.swift
@@ -8,9 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var labelMessage: UILabel!
-    @IBOutlet var labelTapCount: UILabel!
-    @IBOutlet var labelTouchCount: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    
+    var lastPoint: CGPoint!
+    var lineSize: CGFloat = 2.0
+    var lineColor: CGColor = UIColor.red.cgColor
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,23 +21,59 @@ class ViewController: UIViewController {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print(touches)
         let touch = touches.first! as UITouch
-        labelMessage.text = "Touch Started"
-        labelTapCount.text = String(touch.tapCount)
-        labelTouchCount.text = String(touches.count)
+        lastPoint = touch.location(in: imageView)
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        UIGraphicsBeginImageContext(imageView.frame.size)
+        UIGraphicsGetCurrentContext()?.setStrokeColor(lineColor)
+        UIGraphicsGetCurrentContext()?.setLineWidth(lineSize)
+        UIGraphicsGetCurrentContext()?.setLineCap(.round)
+        
         let touch = touches.first! as UITouch
-        labelMessage.text = "Touch Moved"
-        labelTapCount.text = String(touch.tapCount)
-        labelTouchCount.text = String(touches.count)
+        let currentPoint = touch.location(in: imageView)
+        
+        imageView.image?.draw(in: CGRect(x: 0, y: 0, width: imageView.frame.size.width, height: imageView.frame.size.height))
+        
+        UIGraphicsGetCurrentContext()?.move(to: CGPoint(x: lastPoint.x, y: lastPoint.y))
+        UIGraphicsGetCurrentContext()?.addLine(to: CGPoint(x: currentPoint.x, y: currentPoint.y))
+        UIGraphicsGetCurrentContext()?.strokePath()
+        
+        imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        lastPoint = currentPoint
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let touch = touches.first! as UITouch
-        labelMessage.text = "Touch Finished"
-        labelTapCount.text = String(touch.tapCount)
-        labelTouchCount.text = String(touches.count)
+        UIGraphicsBeginImageContext(imageView.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        
+        context.setStrokeColor(lineColor)
+        context.setLineWidth(lineSize)
+        context.setLineCap(.round)
+        
+        imageView.image?.draw(in: CGRect(x: 0, y: 0, width: imageView.frame.width, height: imageView.frame.height))
+        
+        context.move(to: CGPoint(x: lastPoint.x, y: lastPoint.y))
+        context.addLine(to: CGPoint(x: lastPoint.x, y: lastPoint.y))
+        
+        context.strokePath()
+        
+        imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            imageView.image = nil
+        }
+    }
+    
+    
+    @IBAction func handleClear(_ sender: UIButton) {
+        imageView.image = nil
     }
 }
