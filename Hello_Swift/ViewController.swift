2ec40db768110b713457586a0afca9ea3deced75
Jay Park <jhpark.main@gmail.com>
2022-07-31 13:56:55 +0900

    Challenge: Swipe Page Control

--- Hello_Swift/ViewController.swift
+++ Hello_Swift/ViewController.swift
@@ -8,130 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var arrowUpImage: UIImageView!
-    @IBOutlet var arrowLeftImage: UIImageView!
-    @IBOutlet var arrowRightImage: UIImageView!
-    @IBOutlet var arrowDownImage: UIImageView!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var pageControl: UIPageControl!
     
-    let NUMOFTOUCH = 2
-    var arrowUp = [UIImage]()
-    var arrowLeft = [UIImage]()
-    var arrowRight = [UIImage]()
-    var arrowDown = [UIImage]()
+    var images = ["01.png", "02.png", "03.png", "04.png", "05.png", "06.png"]
+    var currentIndex = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        arrowUp.append(UIImage(named: "arrow-up-black.png")!)
-        arrowUp.append(UIImage(named: "arrow-up-red.png")!)
-        arrowUp.append(UIImage(named: "arrow-up-green.png")!)
+        imageView.image = UIImage(named: images[currentIndex])
+        pageControl.currentPage = currentIndex
+        pageControl.numberOfPages = images.count
+        pageControl.hidesForSinglePage = true
+        pageControl.pageIndicatorTintColor = UIColor.lightGray
+        pageControl.currentPageIndicatorTintColor = UIColor.blue
         
-        arrowLeft.append(UIImage(named: "arrow-left-black.png")!)
-        arrowLeft.append(UIImage(named: "arrow-left-red.png")!)
-        arrowLeft.append(UIImage(named: "arrow-left-green.png")!)
-        
-        arrowRight.append(UIImage(named: "arrow-right-black.png")!)
-        arrowRight.append(UIImage(named: "arrow-right-red.png")!)
-        arrowRight.append(UIImage(named: "arrow-right-green.png")!)
-        
-        arrowDown.append(UIImage(named: "arrow-down-black.png")!)
-        arrowDown.append(UIImage(named: "arrow-down-red.png")!)
-        arrowDown.append(UIImage(named: "arrow-down-green.png")!)
-        
-        arrowUpImage.image = arrowUp[0]
-        arrowLeftImage.image = arrowLeft[0]
-        arrowRightImage.image = arrowRight[0]
-        arrowDownImage.image = arrowDown[0]
-        
-        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
-        swipeUp.direction = UISwipeGestureRecognizer.Direction.up
-        self.view.addGestureRecognizer(swipeUp)
-        
-        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
         swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
         self.view.addGestureRecognizer(swipeLeft)
         
-        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
         swipeRight.direction = UISwipeGestureRecognizer.Direction.right
         self.view.addGestureRecognizer(swipeRight)
-        
-        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
-        swipeUp.direction = UISwipeGestureRecognizer.Direction.down
-        self.view.addGestureRecognizer(swipeDown)
-        
-        let multiSwipeUp = UISwipeGestureRecognizer(target: self, action: #selector(respondToMultiSwipeGesture(_:)))
-        multiSwipeUp.direction = UISwipeGestureRecognizer.Direction.up
-        multiSwipeUp.numberOfTouchesRequired = NUMOFTOUCH
-        self.view.addGestureRecognizer(multiSwipeUp)
-        
-        let multiSwipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(respondToMultiSwipeGesture(_:)))
-        multiSwipeLeft.direction = UISwipeGestureRecognizer.Direction.left
-        multiSwipeLeft.numberOfTouchesRequired = NUMOFTOUCH
-        self.view.addGestureRecognizer(multiSwipeLeft)
-        
-        let multiSwipeDown = UISwipeGestureRecognizer(target: self, action: #selector(respondToMultiSwipeGesture(_:)))
-        multiSwipeDown.direction = UISwipeGestureRecognizer.Direction.down
-        multiSwipeDown.numberOfTouchesRequired = NUMOFTOUCH
-        self.view.addGestureRecognizer(multiSwipeDown)
-        
-        let multiSwipeRight = UISwipeGestureRecognizer(target: self, action: #selector(respondToMultiSwipeGesture(_:)))
-        multiSwipeRight.direction = UISwipeGestureRecognizer.Direction.right
-        multiSwipeRight.numberOfTouchesRequired = NUMOFTOUCH
-        self.view.addGestureRecognizer(multiSwipeRight)
     }
     
-    
-    
-    @objc func respondToSwipeGesture(_ gesture: UIGestureRecognizer) {
+    @objc func handleSwipe(_ gesture: UIGestureRecognizer) {
         if let swipeGesture = gesture as? UISwipeGestureRecognizer {
-            print(swipeGesture)
-            arrowUpImage.image = arrowUp[0]
-            arrowLeftImage.image = arrowLeft[0]
-            arrowRightImage.image = arrowRight[0]
-            arrowDownImage.image = arrowDown[0]
             switch swipeGesture.direction {
-            case UISwipeGestureRecognizer.Direction.up:
-                print(1)
-                arrowUpImage.image = arrowUp[1]
-            case UISwipeGestureRecognizer.Direction.left:
-                print(2)
-                arrowLeftImage.image = arrowLeft[1]
             case UISwipeGestureRecognizer.Direction.right:
-                print(3)
-                arrowRightImage.image = arrowRight[1]
-            case UISwipeGestureRecognizer.Direction.down:
-                print(4)
-                arrowDownImage.image = arrowDown[1]
+                
+                if currentIndex > 0 {
+                    currentIndex -= 1
+                    pageControl.currentPage = currentIndex
+                    imageView.image = UIImage(named: images[currentIndex])
+                }
+            case UISwipeGestureRecognizer.Direction.left:
+                
+                if currentIndex < images.count - 1 {
+                    currentIndex += 1
+                    pageControl.currentPage = currentIndex
+                    imageView.image = UIImage(named: images[currentIndex])
+                }
             default:
                 break
             }
         }
     }
     
-    @objc func respondToMultiSwipeGesture(_ gesture: UIGestureRecognizer) {
-        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
-            print(swipeGesture)
-            arrowUpImage.image = arrowUp[0]
-            arrowLeftImage.image = arrowLeft[0]
-            arrowRightImage.image = arrowRight[0]
-            arrowDownImage.image = arrowDown[0]
-            switch swipeGesture.direction {
-            case UISwipeGestureRecognizer.Direction.up:
-                print(1)
-                arrowUpImage.image = arrowUp[2]
-            case UISwipeGestureRecognizer.Direction.left:
-                print(2)
-                arrowLeftImage.image = arrowLeft[2]
-            case UISwipeGestureRecognizer.Direction.right:
-                print(3)
-                arrowRightImage.image = arrowRight[2]
-            case UISwipeGestureRecognizer.Direction.down:
-                print(4)
-                arrowDownImage.image = arrowDown[2]
-            default:
-                break
-            }
-        }
+    @IBAction func handlePaging(_ sender: UIPageControl) {
+        imageView.image = UIImage(named: images[sender.currentPage])
     }
-}
+ }
